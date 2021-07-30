e7b50bf32fe7295a6930755c09991bf41ec633bf
Blake Eriks <eblake98@vt.edu>
2021-07-29 19:45:11 -1000

    changing to vanilla css/html to get core functionality working

--- src/workout/WorkoutCreate.js
+++ src/workout/WorkoutCreate.js
@@ -1,18 +1,17 @@
 import React, {useState} from 'react';
 
-import DateFnsUtils from '@date-io/date-fns';
-
-import {MuiPickersUtilsProvider, KeyboardDatePicker} from '@material-ui/pickers';
-import TextField from '@material-ui/core/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
-
 const WorkoutCreate = props => {
 
     const [editMode, setEditMode] = useState(false);
-    const [workoutType, setWorkoutType] = useState('');
-    const [date, setDate] = useState('');
+    const [workoutType, setWorkoutType] = useState('Push');
+    const [date, setDate] = useState(new Date());
 
     const workoutTypes = ['Push', 'Pull', 'Legs'];
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(date + ' ' + workoutType);
+    }
 
     return (
         !editMode ?
@@ -21,38 +20,13 @@ const WorkoutCreate = props => {
         </div>
         :
         <div className='workout-create-form'>
-            <form>
-                <label>
-                    <div className="workout-date-picker">
-                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                            <KeyboardDatePicker
-                                disableToolbar
-                                variant="inline"
-                                format="MM/dd/yyyy"
-                                margin="normal"
-                                id="date-picker-inline"
-                                label="Date"
-                                KeyboardButtonProps={{
-                                    'aria-label': 'change date',
-                                }}
-                            />
-                        </MuiPickersUtilsProvider>
-                        <TextField
-                            id="standard-select-currency"
-                            select
-                            label="Select"
-                            value={workoutType}
-                            onChange={setWorkoutType}
-                            helperText="Please select your currency"
-                            >
-                            {workoutTypes.map(option => (
-                                <MenuItem key={option} value={option}>
-                                {option}
-                                </MenuItem>
-                            ))}
-                        </TextField>
-                    </div>
-                </label>
+            <form onSubmit={handleSubmit}>
+                <input type="date" value={date} onChange={event => setDate(event.target.value)}/>
+                <select value={workoutType} onChange={event => setWorkoutType(event.target.value)}>
+                    <option>Push</option>
+                    <option>Pull</option>
+                    <option>Legs</option>
+                </select>
                 <input type="submit" value="Submit" />
             </form>
         </div>
